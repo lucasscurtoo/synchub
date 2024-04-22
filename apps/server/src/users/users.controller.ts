a0c44e4fd0b317b4031e9a974675baf8fdd5cb6e
lucasscurtoo <lucascurtoo@gmail.com>
2024-04-22 19:44:25 -0300

    Adding validations for the user picture

--- apps/server/src/users/users.controller.ts
+++ apps/server/src/users/users.controller.ts
@@ -11,10 +11,6 @@ import {
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { v2 as cloudinary } from 'cloudinary';
-import { writeFileSync } from 'fs';
-import { tmpdir } from 'os';
-import { join } from 'path';
 
 @Controller('users')
 export class UsersController {
@@ -34,26 +30,10 @@ export class UsersController {
   @UseInterceptors(FileInterceptor('profilePicture'))
   async update(
     @Param('id') id: string,
-    @UploadedFile() file,
+    @UploadedFile() file: Express.Multer.File,
     @Body() updateUserDto: UpdateUserDto,
   ) {
-    console.log('file', file);
-    if (file) {
-      // Save the uploaded file to a temporary file
-      // so that we can upload it to Cloudinary
-      const tempFilePath = join(tmpdir(), file.originalname);
-      writeFileSync(tempFilePath, file.buffer);
-
-      // Upload the file to Cloudinary
-      const result = await cloudinary.uploader.upload(tempFilePath, {
-        public_id: file.filename,
-        folder: 'profile-pictures',
-      });
-
-      // Save the URL of the uploaded image to updateUserDto.profilePicture
-      updateUserDto.profilePicture = result.url;
-    }
-    return this.usersService.update(id, updateUserDto);
+    return this.usersService.update(id, file, updateUserDto);
   }
 
   @Delete(':id')
